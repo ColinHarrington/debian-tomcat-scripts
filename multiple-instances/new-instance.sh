4ae8c4ead1fa597152aaf029ad56487ce0b00b2b
bpontarelli <bpontarelli@7589a92b-d149-0410-8731-cd42004992f6>
2009-07-10 21:56:24 +0000

    Refactoring to prepare for 6.0 support
    git-svn-id: http://debian-tomcat-scripts.googlecode.com/svn/trunk@12 7589a92b-d149-0410-8731-cd42004992f6

--- multiple-instances/new-instance.sh
+++ multiple-instances/new-instance.sh
@@ -26,7 +26,6 @@ host_name=$1
 port=$2
 jk_port=$3
 host_name_underscores=${host_name//\./_}
-domain_name=$(${script_dir}/domain-name.rb ${host_name})
 instance_dir=/var/lib/tomcat5.5/instances/${host_name}
 log_dir=/var/log/tomcat5.5/instances/${host_name}
 
@@ -39,23 +38,23 @@ mkdir -p ${instance_dir}
 
 # Copy the files and token replace
 mkdir ${instance_dir}/conf ${instance_dir}/webapps ${instance_dir}/bin ${instance_dir}/temp ${instance_dir}/work
-if ! cp ${script_dir}/web.xml ${instance_dir}/conf; then
-  echo "Unable to create new instance ${host_name} because ${script_dir}/web.xml doesn't appear to exist"
+if ! cp ${script_dir}/tomcat5.5/web.xml ${instance_dir}/conf; then
+  echo "Unable to create new instance ${host_name} because ${script_dir}/tomcat5.5/web.xml doesn't appear to exist"
   exit 1
 fi
 
-if ! sed "s/@HOST_NAME@/${host_name}/g" ${script_dir}/server.xml | sed "s/@HOST_NAME_UNDERSCORES@/${host_name_underscores}/g" | sed "s/@PORT@/${port}/g" | sed "s/@JK_PORT@/${jk_port}/g" > ${instance_dir}/conf/server.xml; then
-  echo "Unable to create new instance ${host_name} because ${script_dir}/server.xml doesn't appear to exist"
+if ! sed "s/@HOST_NAME@/${host_name}/g" ${script_dir}/tomcat5.5/server.xml | sed "s/@HOST_NAME_UNDERSCORES@/${host_name_underscores}/g" | sed "s/@PORT@/${port}/g" | sed "s/@JK_PORT@/${jk_port}/g" > ${instance_dir}/conf/server.xml; then
+  echo "Unable to create new instance ${host_name} because ${script_dir}/tomcat5.5/server.xml doesn't appear to exist"
   exit 1
 fi
 
-if ! sed "s/@HOST_NAME@/${host_name}/g" ${script_dir}/tomcat.sh > ${instance_dir}/bin/tomcat.sh; then
-  echo "Unable to find the custom tomcat.sh file in ${script_dir}. This file must exist."
+if ! sed "s/@HOST_NAME@/${host_name}/g" ${script_dir}/tomcat5.5/init-tomcat.sh > ${instance_dir}/bin/init-tomcat.sh; then
+  echo "Unable to find the custom tomcat5.5/init-tomcat.sh file in ${script_dir}. This file must exist."
   exit 1
 fi
 
 # Add the Mod_JK worker defition
-if ! sed "s/@HOST_NAME@/${host_name}/g" ${script_dir}/workers.properties | sed "s/@JK_PORT@/${jk_port}/g" >> /etc/libapache2-mod-jk/workers.properties; then
+if ! sed "s/@HOST_NAME@/${host_name}/g" ${script_dir}/apache2/workers.properties | sed "s/@JK_PORT@/${jk_port}/g" >> /etc/libapache2-mod-jk/workers.properties; then
   echo "Unable to create new instance ${host_name} because the /etc/libapache2-mod-jk/workers.properties file couldn't be updated. Assuming machine doesn't use MOD JK."
 else
   if ! sed "s/\(worker\.list.*\)/\1,${host_name}/g" /etc/libapache2-mod-jk/workers.properties > /tmp/workers.properties; then
@@ -69,7 +68,7 @@ else
   fi
 
   # Setup apache configuration
-  if ! sed "s/@HOST_NAME@/${host_name}/g" ${script_dir}/apache2-conf | sed "s/@DOMAIN_NAME@/${domain_name}/g" | sed "s/@HOST_NAME_UNDERSCORES@/${host_name_underscores}/g" > /etc/apache2/sites-available/${host_name}; then
+  if ! sed "s/@HOST_NAME@/${host_name}/g" ${script_dir}/apache2/site-conf > /etc/apache2/sites-available/${host_name}; then
     echo "Unable to setup Apache2 configuration for the new Tomcat instance"
     exit 1
   fi
@@ -80,7 +79,7 @@ else
   fi
 
   if [ ! -f /etc/apache2/conf.d/mod_jk ]; then
-    if ! cp ${script_dir}/mod_jk /etc/apache2/conf.d; then
+    if ! cp ${script_dir}/apache2/mod_jk /etc/apache2/conf.d; then
       echo "Unable to copy mod_jk configuration to /etc/apache/conf.d. You will have to do this manually."
     fi
   fi
@@ -90,7 +89,7 @@ fi
 chown -R tomcat55:nogroup ${instance_dir}
 chmod -R o-rwx ${instance_dir}
 chmod -R g+w ${instance_dir}
-chmod ug+rx ${instance_dir}/bin/tomcat.sh
+chmod ug+rx ${instance_dir}/bin/init-tomcat.sh
 
 # Setup the logs
 mkdir ${log_dir}
@@ -102,13 +101,13 @@ ln -s ${log_dir} ${instance_dir}/logs
 # Create catalina.policy (for the security manager)
 echo "// This file is an example of a policy file. You can edit this file for the specific instance" > ${instance_dir}/conf/catalina.policy.example
 echo ""  >> ${instance_dir}/conf/catalina.policy.example
-cat ${script_dir}/policy.d/*.policy >> ${instance_dir}/conf/catalina.policy.example
+cat ${script_dir}/tomcat5.5/policy.d/*.policy >> ${instance_dir}/conf/catalina.policy.example
 chown tomcat55:nogroup ${instance_dir}/conf/catalina.policy.example
 chmod o-rwx ${instance_dir}/conf/catalina.policy.example
 chmod g+w ${instance_dir}/conf/catalina.policy.example
 
 # Setup auto start
-cp ${instance_dir}/bin/tomcat.sh /etc/init.d/tomcat5.5_${host_name}
+cp ${instance_dir}/bin/init-tomcat.sh /etc/init.d/tomcat5.5_${host_name}
 update-rc.d tomcat5.5_${host_name} defaults 90
 
 # Create the web directory
